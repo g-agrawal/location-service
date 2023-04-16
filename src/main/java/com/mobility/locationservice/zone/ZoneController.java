b612f580c2a802d276cea0ad6cc9a332ce8bd826
gourav <er.gourav.agrawal@gmail.com>
2023-04-16 12:03:57 +0530

    added logger

--- src/main/java/com/mobility/locationservice/zone/ZoneController.java
+++ src/main/java/com/mobility/locationservice/zone/ZoneController.java
@@ -1,6 +1,8 @@
 package com.mobility.locationservice.zone;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -22,9 +24,12 @@ public class ZoneController {
     @Autowired
     private ZoneService zoneService;
 
+    Logger log = LoggerFactory.getLogger(ZoneController.class);
+
     @CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "Requestor-Type")
     @GetMapping("/zones/nearBy")
     public ResponseEntity<List<Zone>> getNearByZones(@RequestParam double lat, @RequestParam double lng) {
+        log.info("Request received for lat/lng");
         List<Zone> zones = zoneService.getNearestZones(lat, lng);
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-Get-Header", "ExampleHeader");
